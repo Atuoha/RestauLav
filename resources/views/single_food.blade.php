3053ac686d73c3d0262f00d75b052aa404d268ff
Atuoha <atuohainitiatives@gmail.com>
2020-09-15 15:48:58 -0700

    Disqus init-single:

--- resources/views/single_food.blade.php
+++ resources/views/single_food.blade.php
@@ -85,10 +85,28 @@
             <small> - Food Plan: {{ $dish->food_plan }}</small>     
             <small> - Author: {{ $dish->user->name }}</small>
 
-            <div class="row">
                 <!-- DISQUS -->
+                <div id="disqus_thread"></div>
+                <script>
 
-            </div>        
+                /**
+                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+                */
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://restaulav.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+                </script>
+
+                <!--  -->
           </div>
 
       </div>  
